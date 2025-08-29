35869bfd551461d36e0be52b2d78a72f8b730c30
WuHou11 <23110211@student.hcmute.edu.vn>
2025-08-29 10:24:26 +0700

    delete

--- hau.vn/src/main/java/hau/vn/dao/UserDao.java
+++
@@ -1,7 +0,0 @@
-package hau.vn.dao;
-
-import hau.vn.model.*;
-
-public interface UserDao {
-	User get(String username);
-}
--- hau.vn/src/main/java/hau/vn/service/UserService.java
+++
@@ -1,8 +0,0 @@
-package hau.vn.service;
-
-import hau.vn.model.*;
-
-public interface UserService {
-	User login(String username, String password);
-	User get(String username);
-}
